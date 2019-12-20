819d3ec753d02186a45a1508b857ab9a88f3988c
Rahul Arora <seleniumcoaching@gmail.com>
2019-12-20 17:41:22 +0530

    forth commit with mail api

--- src/test/java/com/w2a/APITestingFramework/listeners/ExtentListeners.java
+++ src/test/java/com/w2a/APITestingFramework/listeners/ExtentListeners.java
@@ -1,8 +1,15 @@
 package com.w2a.APITestingFramework.listeners;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -13,11 +20,13 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.w2a.APITestingFramework.utilities.MonitoringMail;
+import com.w2a.APITestingFramework.utilities.TestConfig;
 
 
 
 
-public class ExtentListeners implements ITestListener {
+public class ExtentListeners implements ITestListener, ISuiteListener {
 
 	static Date d = new Date();
 	static String fileName = "Extent_" + d.toString().replace(":", "_").replace(" ", "_") + ".html";
@@ -25,7 +34,7 @@ public class ExtentListeners implements ITestListener {
 	private static ExtentReports extent = ExtentManager.createInstance(System.getProperty("user.dir")+"\\reports\\"+fileName);
 	
 	public static ThreadLocal<ExtentTest> testReport = new ThreadLocal<ExtentTest>();
-	
+	static String messageBody;
 
 	public void onTestStart(ITestResult result) {
 
@@ -91,6 +100,36 @@ public class ExtentListeners implements ITestListener {
 			extent.flush();
 		}
 
+	}
+
+	public void onStart(ISuite suite) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onFinish(ISuite suite) {
+		
+
+
+		try {
+			 messageBody = "http://"+InetAddress.getLocalHost().getHostAddress()+":8080/job/APITestingFramework/Extent_20Reports/"+fileName;
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		MonitoringMail mail = new MonitoringMail();
+		try {
+			mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 	}
 
 }
